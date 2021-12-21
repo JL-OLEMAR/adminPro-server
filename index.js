8ebb88b06ad1a0534841f8592cc0711fe5038b33
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-21 12:14:00 -0500

    First route

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express')
 // Crear el servidor de express
 const app = express()
 
+// Rutas
+app.get('/', (req, res) => {
+  res.json({
+    ok: true,
+    msg: 'Hola mundo'
+  })
+})
+
 app.listen(3000, () => {
   console.log('Server corriendo en: http://localhost:' + 3000)
 })
