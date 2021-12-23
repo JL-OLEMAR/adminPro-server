9eb2136e70f815bbc17a9179aa045d0e87b2cb12
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-22 22:34:21 -0500

    Conf cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express')
 require('dotenv').config()
+const express = require('express')
 const { dbConnection } = require('./db/config.js')
+const cors = require('cors')
 
 // Crear el servidor de express
 const app = express()
+
+// Config cors
+app.use(cors())
 
 // Base de datos
 dbConnection()
