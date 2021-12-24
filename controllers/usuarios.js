8a1711936037904e97b21bb043d3e2ebf91481e4
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-23 22:27:45 -0500

    Validar email unico

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const Usuario = require('../models/usuario.js')
 
 const getUsers = async (req, res) => {
@@ -9,16 +10,32 @@ const getUsers = async (req, res) => {
   })
 }
 
-const newUser = async (req, res) => {
+const newUser = async (req, res = response) => {
   const { nombre, password, email } = req.body
-  const usuario = new Usuario({ nombre, password, email })
 
-  await usuario.save()
+  try {
+    const existeEmail = await Usuario.findOne({ email })
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El correo ya existe'
+      })
+    }
 
-  res.json({
-    ok: true,
-    usuario
-  })
+    const usuario = new Usuario({ nombre, password, email })
+    await usuario.save()
+
+    res.json({
+      ok: true,
+      usuario
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Error contacte al administrador'
+    })
+  }
 }
 
 module.exports = { getUsers, newUser }
