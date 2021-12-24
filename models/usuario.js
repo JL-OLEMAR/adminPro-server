1ff4d3739c5ad98e254fa786594fc77dd2c3eb23
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-23 22:09:27 -0500

    Change method UsuarioSchema

--- models/usuario.js
+++ models/usuario.js
@@ -28,4 +28,10 @@ const UsuarioSchema = Schema({
   }
 })
 
+UsuarioSchema.method('toJSON', function () {
+  const { __v, _id, ...object } = this.toObject()
+  object.uid = _id
+  return object
+})
+
 module.exports = model('Usuario', UsuarioSchema)
