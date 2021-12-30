21c79064faac386d85bd689dee040a6b01b9d501
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-29 23:33:26 -0500

    Busquedas por nombre en todas las colecciones

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -1,14 +1,26 @@
 const { response } = require('express')
 const Usuario = require('../models/usuario.js')
+const Medico = require('../models/medico.js')
+const Hospital = require('../models/hospital.js')
 
 const getAll = async (req, res = response) => {
-  const query = req.params.busqueda
+  const busqueda = req.params.busqueda
+  /* i = case insensitive, signica que busca por todo el texto,
+  mayusculas, minusculas, espacios entre el campo, etc
+  (todo lo que esta dentro del campo a buscar) */
+  const regex = new RegExp(busqueda, 'i')
 
-  const usuarios = await Usuario.find({ nombre: query })
+  const [usuarios, medicos, hospitales] = await Promise.all([
+    Usuario.find({ nombre: regex }),
+    Medico.find({ nombre: regex }),
+    Hospital.find({ nombre: regex })
+  ])
 
   res.json({
     ok: true,
-    usuarios
+    usuarios,
+    medicos,
+    hospitales
   })
 }
 
