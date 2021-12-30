3ab8eaea63b1412caede63c3aff88595d1bbefee
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-29 23:15:26 -0500

    Buscar todos por nombre

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -1,12 +1,14 @@
 const { response } = require('express')
+const Usuario = require('../models/usuario.js')
 
 const getAll = async (req, res = response) => {
   const query = req.params.busqueda
 
+  const usuarios = await Usuario.find({ nombre: query })
+
   res.json({
     ok: true,
-    msg: 'Todo bien',
-    query
+    usuarios
   })
 }
 
