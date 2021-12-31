c282474c3a6878e924bf277994bc12324c42a91e
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-30 23:55:29 -0500

    Busqueda por coleccion especifica

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -24,6 +24,49 @@ const getAll = async (req, res = response) => {
   })
 }
 
+const getDocumentosColeccion = async (req, res = response) => {
+  const tabla = req.params.tabla
+  const busqueda = req.params.busqueda
+  /* i = case insensitive, signica que busca por todo el texto,
+  mayusculas, minusculas, espacios entre el campo, etc
+  (todo lo que esta dentro del campo a buscar) */
+  const regex = new RegExp(busqueda, 'i')
+
+  let data = []
+
+  switch (tabla) {
+    case 'medicos':
+      data = await Medico
+        .find({ nombre: regex })
+        .populate('usuario', 'nombre img')
+        .populate('hospital', 'nombre img')
+
+      break
+
+    case 'hospitales':
+      data = await Hospital
+        .find({ nombre: regex })
+        .populate('usuario', 'nombre img')
+      break
+
+    case 'usuarios':
+      data = await Usuario.find({ nombre: regex })
+      break
+
+    default:
+      return res.status(400).json({
+        ok: false,
+        msg: 'La tabla tiene que ser: usuarios, médicos o hospitales'
+      })
+  }
+
+  return res.json({
+    ok: true,
+    resultados: data
+  })
+}
+
 module.exports = {
-  getAll
+  getAll,
+  getDocumentosColeccion
 }
--- routes/busquedas.js
+++ routes/busquedas.js
@@ -1,12 +1,13 @@
 /*
-  Ruta: /api/todo/:busqueda
+  Ruta: /api/todo
 */
 const { Router } = require('express')
 const { validarJWT } = require('../middlewares/validar-jwt.js')
-const { getAll } = require('../controllers/busquedas.js')
+const { getAll, getDocumentosColeccion } = require('../controllers/busquedas.js')
 
 const router = Router()
 
 router.get('/:busqueda', validarJWT, getAll)
+router.get('/coleccion/:tabla/:busqueda', validarJWT, getDocumentosColeccion)
 
 module.exports = router
