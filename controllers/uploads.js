b62942b1d90b13f868b108b8ff4468e5bb4ba67d
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-31 15:38:57 -0500

    Subir imagenes al servidor

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,5 +1,5 @@
 const { response } = require('express')
-// const Hospital = require('../models/hospital.js')
+const { v4: uuidv4 } = require('uuid')
 
 const fileUpload = (req, res = response) => {
   const coleccion = req.params.coleccion
@@ -23,10 +23,47 @@ const fileUpload = (req, res = response) => {
   }
 
   // Procesar la imagen...
+  const file = req.files.imagen
+
+  // Tomar la extensión del archivo, ejemplo: .png .jpg etc
+  const nombreCortado = file.name.split('.')
+  const extensionArchivo = nombreCortado[nombreCortado.length - 1]
+
+  // Validar que la extensión sea válida
+  const extensionesValidas = ['png', 'jpg', 'gif', 'jpeg']
+  if (!extensionesValidas.includes(extensionArchivo)) {
+    return res.status(400).json({
+      ok: false,
+      msg: 'No es una extensión válida'
+    })
+  }
+
+  // Generar el nombre del archivo
+  const nombreArchivo = `${uuidv4()}.${extensionArchivo}`
+
+  // Path donde se guardará el archivo
+  const path = `./uploads/${coleccion}/${nombreArchivo}`
+
+  // Mover la imagen
+  file.mv(path, err => {
+    if (err) {
+      console.log(err)
+      return res.status(500).json({
+        ok: false,
+        msg: 'Error al mover la imagen'
+      })
+    }
+
+    res.json({
+      ok: true,
+      msg: 'Archivo subido correctamente',
+      nombreArchivo
+    })
+  })
 
   res.json({
     ok: true,
-    msg: 'Imagen subida correctamente',
+    nombreArchivo,
     id
   })
 }
