6e32c5598bfe0744cabe320049057cf41ea2dd9c
JL-OLEMAR <joseluis19963@gmail.com>
2022-01-26 19:43:17 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const { dbConnection } = require('./db/config.js')
 
 // Crear el servidor de express
@@ -25,6 +26,11 @@ app.use('/api/medicos', require('./routes/medicos.js'))
 app.use('/api/login', require('./routes/auth.js'))
 app.use('/api/todo', require('./routes/busquedas.js'))
 app.use('/api/upload', require('./routes/uploads.js'))
+
+// Any route not found
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server corriendo en: http://localhost:${process.env.PORT}`)
